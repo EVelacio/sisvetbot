9062b40bd76cd5f183970ee30fd98553ddac4e45
EVelacio <velacio@outlook.com>
2019-05-17 11:25:29 -0500

    Algunos roles

--- database/seeds/RoleSeed.php
+++ database/seeds/RoleSeed.php
@@ -13,5 +13,14 @@ class RoleSeed extends Seeder
     {
         Bouncer::allow('administrator')->to('users_manage');
         Bouncer::allow('veterinario')->to('crear_cita');
+        Bouncer::allow('veterinario')->to('cancelar_cita');
+        Bouncer::allow('veterinario')->to('agregar_especie');
+        Bouncer::allow('veterinario')->to('eliminar_especie');
+        Bouncer::allow('veterinario')->to('agregar_raza');
+        Bouncer::allow('veterinario')->to('eliminar_raza');
+        Bouncer::allow('cliente')->to('agregar_mascota');
+        Bouncer::allow('cliente')->to('eliminar_sesion');
+
+        
     }
 }
